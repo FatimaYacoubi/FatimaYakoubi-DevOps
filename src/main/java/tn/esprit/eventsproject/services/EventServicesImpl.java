e7a970fbd0f94dd03402bd36d842969f8a66b3de
Fatima <yacoubi.fatima@esprit.tn>
2023-12-12 11:57:47 +0100

    swagger

--- src/main/java/tn/esprit/eventsproject/services/EventServicesImpl.java
+++ src/main/java/tn/esprit/eventsproject/services/EventServicesImpl.java
@@ -76,8 +76,8 @@ public class EventServicesImpl implements IEventServices{
     }
 
     @Override
-    public List<Logistics> getLogisticsDates(LocalDate date_debut, LocalDate date_fin) {
-        List<Event> events = eventRepository.findByDateDebutBetween(date_debut, date_fin);
+    public List<Logistics> getLogisticsDates(LocalDate dateDebut, LocalDate dateFin) {
+        List<Event> events = eventRepository.findByDateDebutBetween(dateDebut, dateFin);
 
         List<Logistics> logisticsList = new ArrayList<>();
         for (Event event:events){
@@ -101,7 +101,6 @@ public class EventServicesImpl implements IEventServices{
     @Override
     public void calculCout() {
         List<Event> events = eventRepository.findByParticipants_NomAndParticipants_PrenomAndParticipants_Tache("Tounsi","Ahmed", Tache.ORGANISATEUR);
-    // eventRepository.findAll();
         float somme = 0f;
         for(Event event:events){
             log.info(event.getDescription());
